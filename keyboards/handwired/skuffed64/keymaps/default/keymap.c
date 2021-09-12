6f86ec6a36860be616bc0654ffd88f4fcd1a803b
uclatommy <tkchen@gmail.com>
2021-09-12 15:59:43 -0700

    Added a 2nd later and fixed layout.

--- keyboards/handwired/skuffed64/keymaps/default/keymap.c
+++ keyboards/handwired/skuffed64/keymaps/default/keymap.c
@@ -18,30 +18,93 @@
 // Defines names for use in layer keycodes and the keymap
 enum layer_names {
     _BASE,
-    _FN
+    _FN1,
+    _FN2
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-    [_BASE] = LAYOUT_65_ansi(
-      KC_ESC,           KC_Q,     KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_PGUP, KC_PGDN, KC_7,    KC_8,    KC_9,    KC_EQL,
-      LCTL_T(KC_CAPS),  KC_A,     KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_BSPC, KC_HOME, KC_END,  KC_4,    KC_5,    KC_6,    KC_MINS,
-      KC_LSFT,          KC_Z,     KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_LSFT, KC_ENT,                    KC_1,    KC_2,    KC_3,
-      KC_LALT,          MO(1),                      KC_SPC,                             KC_LALT, KC_MENU, MO(1),                     KC_SLCK,          KC_0,             KC_DOT,  KC_ENT
+    [_BASE] = LAYOUT(
+      KC_ESC,           KC_Q,     KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_PGDN, KC_END,  KC_7,    KC_8,     KC_9,   KC_EQL,
+      LCTL_T(KC_CAPS),  KC_A,     KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_BSPC, KC_PGUP, KC_HOME, KC_4,    KC_5,     KC_6,   KC_MINS,
+      KC_LSFT,          KC_Z,     KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_LSFT, KC_ENT,                    KC_1,    KC_2,     KC_3,
+      KC_LALT,          MO(_FN2),                   KC_SPC,                             KC_LALT, KC_MENU, MO(_FN1),                  KC_SLCK,          KC_0,    KC_DOT,           KC_ENT
     ),
-    [_FN] = LAYOUT_65_ansi(
-      KC_ESC,           KC_Q,     KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_PGUP, KC_PGDN, KC_7,    KC_8,    KC_9,    KC_EQL,
-      LCTL_T(KC_CAPS),  KC_A,     KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_BSPC, KC_HOME, KC_END,  KC_4,    KC_5,    KC_6,    KC_MINS,
-      KC_LSFT,          KC_Z,     KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_LSFT, KC_ENT,                    KC_1,    KC_2,    KC_3,
-      KC_LALT,          MO(1),                      KC_SPC,                             KC_LALT, KC_MENU, MO(1),                     KC_SLCK,          KC_0,             KC_DOT,  KC_ENT
+    [_FN1] = LAYOUT(
+      KC_GRAVE,         _______,  KC_UP,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_F7,   KC_F8,    KC_F9,  KC_F11,
+      _______,          KC_LEFT,  KC_DOWN, KC_RIGHT,_______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,_______, _______, KC_BRK,  _______, _______, KC_F4,   KC_F5,    KC_F6,  KC_F12,
+      _______,          _______,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                   KC_F1,   KC_F2,    KC_F3,
+      _______,          _______,                    _______,                            _______, _______, _______,                   KC_MUTE,          KC_F10,  _______,          _______
     ),
+    [_FN2] = LAYOUT(
+      KC_GRAVE,         _______,  KC_UP,   _______, _______, _______, _______, _______, _______, _______, _______, KC_9,    KC_0,    _______, _______, _______, _______, _______, _______,
+      _______,          KC_LEFT,  KC_DOWN, KC_RIGHT,_______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,_______, _______, KC_BSLS, _______, _______, _______, KC_UP,   _______, _______,
+      _______,          _______,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                   KC_LEFT, KC_DOWN, KC_RIGHT,
+      _______,          _______,                    _______,                            _______, _______, _______,                   _______,          _______,  _______,         _______
+    ),
+};
+
+uint8_t mod_state;
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    mod_state = get_mods();
+    switch (keycode) {
+
+    case KC_ESC:
+        // Detect the activation of only Left Alt
+        if ((mod_state & MOD_BIT(KC_LALT)) == MOD_BIT(KC_LALT)) {
+            if (record->event.pressed) {
+                // No need to register KC_LALT because it's already active.
+                // The Alt modifier will apply on this KC_TAB.
+                register_code(KC_TAB);
+            } else {
+                unregister_code(KC_TAB);
+            }
+            // Do not let QMK process the keycode further
+            return false;
+        }
+        // Else, let QMK process the KC_ESC keycode as usual
+        return true;
+
+    case KC_BSPC:
+        {
+        // Initialize a boolean variable that keeps track
+        // of the delete key status: registered or not?
+        static bool delkey_registered;
+        if (record->event.pressed) {
+            // Detect the activation of either shift keys
+            if (mod_state & MOD_MASK_SHIFT) {
+                // First temporarily canceling both shifts so that
+                // shift isn't applied to the KC_DEL keycode
+                del_mods(MOD_MASK_SHIFT);
+                register_code(KC_DEL);
+                // Update the boolean variable to reflect the status of KC_DEL
+                delkey_registered = true;
+                // Reapplying modifier state so that the held shift key(s)
+                // still work even after having tapped the Backspace/Delete key.
+                set_mods(mod_state);
+                return false;
+            }
+        } else { // on release of KC_BSPC
+            // In case KC_DEL is still being sent even after the release of KC_BSPC
+            if (delkey_registered) {
+                unregister_code(KC_DEL);
+                delkey_registered = false;
+                return false;
+            }
+        }
+        // Let QMK process the KC_BSPC keycode as usual outside of shift
+        return true;
+      }
+
+    }
+    return true;
 };
 
 uint8_t selected_layer = 0;
 
 bool encoder_update_user(uint8_t index, bool clockwise) {
   switch(get_highest_layer(layer_state)){
-		case 2:
+		case _FN2:
 			if (IS_HOST_LED_ON(USB_LED_SCROLL_LOCK)){
 				if (clockwise) {
 					tap_code(KC_WH_R);
@@ -56,7 +119,7 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
 				}
 			}
 			break;
-		case 1:
+		case _FN1:
 			if (clockwise) {
 				tap_code(KC_VOLU);
 			} else {
--- keyboards/handwired/skuffed64/rules.mk
+++ keyboards/handwired/skuffed64/rules.mk
@@ -29,5 +29,3 @@ RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 MIDI_ENABLE = no            # MIDI support
 BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 AUDIO_ENABLE = no           # Audio output on port C6
-
-LAYOUTS = 65_ansi
