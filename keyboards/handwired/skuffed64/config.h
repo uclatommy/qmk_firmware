e8ec6d8285fb2dd88611cbfc3492736be235c7ad
uclatommy <tkchen@gmail.com>
2021-09-18 13:29:28 -0700

    Using a teensy++ 2.0 and adds led indicators for numlock, caps, and scroll lock.

--- keyboards/handwired/skuffed64/config.h
+++ keyboards/handwired/skuffed64/config.h
@@ -1,4 +1,4 @@
-/*
+ /*
 Copyright 2020 uclatommy
 
 This program is free software: you can redistribute it and/or modify
@@ -40,10 +40,14 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *                  ROW2COL = ROW = Anode (+), COL = Cathode (-, marked on diode)
  *
  */
-#define MATRIX_ROW_PINS { F5, F4, F1, F0 }
-#define MATRIX_COL_PINS { F6, F7, B6, B5, B4, D7, B0, B1, B2, B3, B7, D0, D1, D2, D3, C6, C7, D5, E6 }
-#define ENCODERS_PAD_A { D4 }
-#define ENCODERS_PAD_B { D6 }
+#define MATRIX_ROW_PINS { D4, D3, D0, B7}
+#define MATRIX_COL_PINS { D5, E0, E1, C0, C1, C2, B0, F0, F1, F2, F3, F4, F5, F6, F7, B1, C3, C4, C5 }
+#define ENCODERS_PAD_A { C6 }
+#define ENCODERS_PAD_B { C7 }
+#define LED_NUM_LOCK_PIN B6
+#define LED_CAPS_LOCK_PIN B5
+#define LED_SCROLL_LOCK_PIN B4
+#define LED_PIN_ON_STATE 1
 #define ENCODER_RESOLUTION 1
 #define UNUSED_PINS
 
